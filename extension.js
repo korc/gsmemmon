e8c2bd611a10cd9ccd69c3345b4803a870656d0b
Lauri Korts-Pärn <lauri@cyberdefense.jp>
2018-05-21 16:19:33 -0400

    use /proc/self/ instead of using get_unix_pid

--- extension.js
+++ extension.js
@@ -8,7 +8,6 @@ let buttonLabel;
 let statusFile;
 let timeoutId;
 let tick = 0;
-let pid;
 
 function _reloadShell() {
     global.reexec_self();
@@ -27,9 +26,8 @@ function init() {
     // let icon = new St.Icon({ icon_name: 'system-run-symbolic',
     //                          style_class: 'system-status-icon' });
 
-    pid = (new Gio.Credentials()).get_unix_pid();
-    statusFile = Gio.File.new_for_path('/proc/' + pid + '/status');
-    buttonLabel = new St.Label({text: "pid=" + pid});
+    statusFile = Gio.File.new_for_path('/proc/self/status');
+    buttonLabel = new St.Label({text: "?? MB"});
     // button.set_child(icon);
     button.set_child(buttonLabel);
     button.connect('button-press-event', _reloadShell);
@@ -39,8 +37,9 @@ function updateLabel() {
     tick += 1;
     let fileInput = statusFile.read(null);
     let data = fileInput.read_bytes(8192, null).get_data();
-    let mem = ("" + data).match(/^RssAnon:\s+([0-9]+)/m)[1];
-    buttonLabel.set_text('pid=' + pid + ' mem=' + mem);
+    let pid = ("" + data).match(/^Pid:\s+([0-9]+)/m)[1];
+    let mem = ("" + data).match(/^RssAnon:\s+(.*)$/m)[1];
+    buttonLabel.set_text('['+pid+'] ' + mem);
     return true;
 }
 
